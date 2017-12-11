43713fdf34c50a219e4f74277a0721a2062c904d
Jianhui Zhao <jianhuizhao329@gmail.com>
2017-12-11 12:48:38 +0800

    Configure the proxy to facilitate debugging
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/config/index.js
+++ vuci-ui-base/src/config/index.js
@@ -10,7 +10,12 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+        '/ubus': {
+            target: 'http://lede.lan',
+            changeOrigin: true
+        }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
