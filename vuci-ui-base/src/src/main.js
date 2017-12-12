8b9d3b9d23e866f48bee344972a243717b983866
Jianhui Zhao <jianhuizhao329@gmail.com>
2017-12-12 22:32:27 +0800

    Use vue-axios
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/main.js
+++ vuci-ui-base/src/src/main.js
@@ -5,10 +5,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
