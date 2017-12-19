f09873f8e868032e1d5abfcf72eb1f992e474164
Jianhui Zhao <jianhuizhao329@gmail.com>
2017-12-19 21:07:38 +0800

    Login.vue: Support log in by press the Enter key.
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/pages/Login.vue
+++ vuci-ui-base/src/src/pages/Login.vue
@@ -1,23 +1,24 @@
 <template>
-    <Card class="login-container">
-        <p slot="title">Authorization Required</p>
-        <Form ref="form" :model="form" :rules="ruleValidate">
-            <FormItem prop="username">
-                <Input type="text" v-model="form.username" auto-complete="off" placeholder="Enter username...">
-                    <Icon type="ios-person-outline" slot="prepend"></Icon>
-                </Input>
-            </FormItem>
-            <FormItem>
-                <Input type="password" v-model="form.password" auto-complete="off" placeholder="Enter password...">
-                    <Icon type="ios-locked-outline" slot="prepend"></Icon>
-                </Input>
-            </FormItem>
-            <FormItem>
-                <Button type="primary" style="width:100%;" @click="handleSubmit">Login</Button>
-            </FormItem>
-        </Form>
-        
-    </Card>
+    <div @keydown.enter="handleSubmit">
+        <Card class="login-container">
+            <p slot="title">Authorization Required</p>
+            <Form ref="form" :model="form" :rules="ruleValidate">
+                <FormItem prop="username">
+                    <Input type="text" v-model="form.username" auto-complete="off" placeholder="Enter username...">
+                        <Icon type="ios-person-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem>
+                    <Input type="password" v-model="form.password" auto-complete="off" placeholder="Enter password...">
+                        <Icon type="ios-locked-outline" slot="prepend"></Icon>
+                    </Input>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" style="width:100%;" @click="handleSubmit">Login</Button>
+                </FormItem>
+            </Form>
+        </Card>
+    </div>
 </template>
 
 <script>
