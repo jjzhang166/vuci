6573e7e12881a96b1e4786df82571a9e2133d4d5
Jianhui Zhao <jianhuizhao329@gmail.com>
2017-12-19 22:43:28 +0800

    Optimize code
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/pages/Home.vue
+++ vuci-ui-base/src/src/pages/Home.vue
@@ -13,7 +13,7 @@
             <Col span="20">
                 <div class="layout-header">Header</div>
                 <div class="layout-content">
-                    <div class="layout-content-main">Content</div>
+                    <div>Content</div>
                 </div>
                 <div class="layout-copy">
                     2017 &copy; Jianhui Zhao
@@ -24,40 +24,43 @@
 </template>
 
 <style scoped>
-   .layout{
+   .layout {
         border: 1px solid #d7dde4;
         background: #f5f7f9;
         position: relative;
     }
-    .layout-content{
-        min-height: 400px;
-        margin: 15px;
-        overflow: hidden;
-        background: #fff;
-        border-radius: 4px;
-    }
-    .layout-content-main{
-        padding: 10px;
-    }
-    .layout-copy{
-        text-align: center;
-        padding: 10px 0 20px;
-        color: #9ea7b4;
-    }
-    .layout-menu-left{
-        background: #464c5b;
-    }
-    .layout-header{
-        height: 60px;
-        background: #fff;
-        box-shadow: 0 1px 1px rgba(0,0,0,.1);
-    }
-    .layout-logo-left{
+
+    .layout-logo-left {
         width: 90%;
         height: 30px;
         background: #5b6270;
         border-radius: 3px;
         margin: 15px auto;
+    }
+
+    .layout-menu-left{
+        background: #464c5b;
+    }
+
+    .layout-header {
+        height: 60px;
+        background: #fff;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+    }
+
+    .layout-content {
+        min-height: 400px;
+        margin: 15px;
+        overflow: hidden;
+        background: #fff;
+        border-radius: 4px;
+        padding: 10px;
+    }
+
+    .layout-copy {
+        text-align: center;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
     }
 </style>
 
