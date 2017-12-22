af2cca6c02e0d0813e968e9313e51488d83ce5c3
Jianhui Zhao <jianhuizhao329@gmail.com>
2017-12-22 17:18:23 +0800

    debug

--- vuci-ui-base/src/src/views/status.overview.vue
+++ vuci-ui-base/src/src/views/status.overview.vue
@@ -1,17 +1,7 @@
 <template>
 	<div>
 		<h1>Status</h1>
-		<Card>
-			<p slot="title">System</p>
-			<table width="100%" cellspacing="10">
-				<tr><td width="20%">Hostname</td><td>{{sysinfo.hostname}}</td></tr>
-				<tr><td width="20%">Model</td><td>{{sysinfo.model}}</td></tr>
-				<tr><td width="20%">Kernel Version</td><td>{{sysinfo.kernel}}</td></tr>
-				<tr><td width="20%">Firmware</td><td>{{sysinfo.description}}</td></tr>
-				<tr><td width="20%">Local Time</td><td>{{sysinfo.localtime}}</td></tr>
-				<tr><td width="20%">Uptime</td><td>{{sysinfo.uptime}}</td></tr>
-			</table>
-		</Card>
+        <Table :columns="columns" :data="data"></Table>
 	</div>
 </template>
 
@@ -19,28 +9,32 @@
     export default {
         data () {
             return {
-            	sysinfo: {
-            		hostname: '',
-            		model: '',
-            		kernel: '',
-            		description: '',
-            		localtime: '',
-            		uptime: ''
-            	}
+                columns: [
+                    {
+                        title: 'System',
+                        key: 'name',
+                        width: 200
+                    },
+                    {
+                        title: ' ',
+                        key: 'value'
+                    }
+                ],
+                data: []
             }
         },
 
         mounted: function() {
-        	this.$ubus.call('system', 'board', {}).then((r) => {
-        		this.sysinfo.hostname = r.hostname;
-        		this.sysinfo.model = r.model;
-        		this.sysinfo.kernel = r.kernel;
-        		this.sysinfo.description = r.release.description;
+               	this.$ubus.call('system', 'board', {}).then((r) => {
+                this.data.push({name: 'hostname', value: r.hostname});
+                this.data.push({name: 'model', value: r.model});
+                this.data.push({name: 'kernel', value: r.kernel});
+                this.data.push({name: 'description', value: r.release.description});
         	});
 
         	this.$ubus.call('system', 'info', {}).then((r) => {
-        		this.sysinfo.localtime = (new Date(r.localtime * 1000)).toUTCString();
-        		this.sysinfo.uptime = r.uptime;
+                this.data.push({name: 'localtime', value: (new Date(r.localtime * 1000)).toUTCString()});
+                this.data.push({name: 'uptime', value: r.uptime});
         	});
         }
     }
